c5e4b8cf1718510d69cb748777852aae082bb5ec
Mari3719 <38593675+Mari3719@users.noreply.github.com>
2018-05-01 23:53:01 +0300

    hm1
    hm1

---
+++ hm1.py
@@ -0,0 +1,20 @@
+Python 3.5.0 (v3.5.0:374f501f4567, Sep 13 2015, 02:27:37) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======================== RESTART: C:/Users/Mary/b.py ========================
+Маша
+======================== RESTART: C:/Users/Mary/b.py ========================
+Маша
+======================== RESTART: C:/Users/Mary/b.py ========================
+Hallo World!
+>>> 
+======================== RESTART: C:/Users/Mary/b.py ========================
+Hallo World!
+Как дела?
+2+2=4
+>>> 
+======================== RESTART: C:/Users/Mary/b.py ========================
+тестовый файл
+мои пальцы что-то пишут
+sun,moon
+>>> 
