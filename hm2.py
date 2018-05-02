054ccfa866d9bfa591f1621c02d418d90422390c
Mari3719 <38593675+Mari3719@users.noreply.github.com>
2018-05-02 23:26:31 +0300

    hm2
    hm2

---
+++ hm2.py
@@ -0,0 +1,16 @@
+a=int(input('введите число'))
+b=int(input('введите число'))
+c=int(input('введите число'))
+if (a*b==c):
+    print('да,a*b=c')
+else:
+    print('нет,a*b=c')
+if (b==0):
+    print('деление невозможно')
+else:
+    if (a/b==c):
+        print('да,a/b=c')
+    else:
+        print('нет,a/b=c')
+    
+    
