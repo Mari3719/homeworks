b52cd910f98c1499d51e334f02ddf1226f6572ff
Mari3719 <38593675+Mari3719@users.noreply.github.com>
2018-05-14 01:31:48 +0300

    Add files via upload

---
+++ hm6.py
@@ -0,0 +1,11 @@
+with open("text.txt", "w", encoding="utf-8") as f:
+    number = []
+    for i in range(7):
+        number.append(input("Введите число: "))
+    for i in range(7):
+        for m in range(int(number[i])):
+            f.write("X")
+        f.write("\z")
+with open("text.txt", encoding="utf-8") as f:
+    text = f.read()
+    print(text)
